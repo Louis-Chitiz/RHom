5c4107f296d70c02a1d14daadc6ae9bbb7fb9937
Louis Chitiz <81125060+Louis-Chitiz@users.noreply.github.com>
2026-03-08 18:48:19 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 setup(
-   name='ThoughtSpace',
+   name='RHom',
    version='0.0.1',
    description='Package of functions to run and visualize PCA on ESQ data, and project between datasets.',
-   author='Bronte Mckeown & Will Strawson',
-   author_email='bronte.mckeown@gmail.com',
-   packages=find_packages(include=['ThoughtSpace']),
+   author='Louis Chitiz',
+   author_email='louischitiz@gmail.com',
+   packages=find_packages(include=['RHom']),
    install_requires=required,
-   package_data={'ThoughtSpace': [
+   package_data={'RHom': [
     'fonts/*.ttf'
     ]
     }
